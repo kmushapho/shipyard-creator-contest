ee743d7706e7263cf6611b2acdb56f0ff92b9af4
kmushapho@gmail <kmushapho@gmail.com>
2026-02-12 14:07:11 +0200

    render sh to add cache quick build

--- euphoric_cook/render_build.sh
+++ euphoric_cook/render_build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e  # stop on errors
 
-# Clone Flutter SDK
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
+# Set cache directories
+FLUTTER_DIR="/cache/flutter"
+PUB_CACHE_DIR="/cache/pub-cache"
+
+# Clone Flutter if not cached
+if [ ! -d "$FLUTTER_DIR" ]; then
+    echo "Cloning Flutter SDK..."
+    git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$FLUTTER_DIR"
+else
+    echo "Using cached Flutter SDK"
+fi
 
 # Add Flutter to PATH
-export PATH="$PWD/flutter/bin:$PATH"
+export PATH="$FLUTTER_DIR/bin:$PATH"
+
+# Use cached pub packages if available
+export PUB_CACHE="$PUB_CACHE_DIR"
 
 # Get dependencies
 flutter pub get
